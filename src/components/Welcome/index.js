c06253528d5b0a97a9f6bb4e368b22cf4a62e11a
rakesh <rakeshsagartfi21@gmail.com>
2025-06-10 15:07:41 +0000

    Added Function to button

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -5,14 +5,29 @@ import {Component} from 'react'
 import './index.css'
 
 class Welcome extends Component {
+  state = {
+    isSubscribed: false
+  }
+
+  onClickSubscribeButton = () =>{
+    this.setState((prevState) => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () =>{
+    const {isSubscribed} = this.state 
+
+    return isSubscribed? 'Subscribed' : 'Subscribe'
+  }
+
   render() {
+    const buttonText = this.getButtonText()
     return (
       <div className="welcome-app-container">
         <h1 className="heading"> Welcome </h1>
         <p className="description">Thank you! Happy Learning </p>
-        <button type="button" className="subscribe-button">
+        <button type="button" className="subscribe-button" onClick={this.onClickSubscribeButton}>
           {' '}
-          Subscribe{' '}
+          {buttonText}{' '}
         </button>
       </div>
     )
@@ -20,3 +35,4 @@ class Welcome extends Component {
 }
 
 export default Welcome
+ 
