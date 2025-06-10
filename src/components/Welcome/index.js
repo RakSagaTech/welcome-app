38b15ee135738ccc93e386a4a81c02bc8c0d8acc
rakesh <rakeshsagartfi21@gmail.com>
2025-06-10 14:01:24 +0000

    Added Content(No CSS)

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,20 @@
+// Write your code here
+
+import {Component} from 'react'
+
+import './index.css'
+
+
+class Welcome extends Component{
+    render(){
+        return (
+            <div>
+                <h1> Welcome </h1>
+                <p>Thank you! Happy Learning </p>
+                <button type="button"> Subscribe </button>
+            </div>
+        )
+    }
+}
+
+export default Welcome
