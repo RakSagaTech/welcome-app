a2dc95e729da1dc654f0156d91387e3592baacd9
rakesh <rakeshsagartfi21@gmail.com>
2025-06-10 14:57:10 +0000

    Added CSS

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -4,17 +4,19 @@ import {Component} from 'react'
 
 import './index.css'
 
-
-class Welcome extends Component{
-    render(){
-        return (
-            <div>
-                <h1> Welcome </h1>
-                <p>Thank you! Happy Learning </p>
-                <button type="button"> Subscribe </button>
-            </div>
-        )
-    }
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="welcome-app-container">
+        <h1 className="heading"> Welcome </h1>
+        <p className="description">Thank you! Happy Learning </p>
+        <button type="button" className="subscribe-button">
+          {' '}
+          Subscribe{' '}
+        </button>
+      </div>
+    )
+  }
 }
 
 export default Welcome
